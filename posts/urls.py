9fdfacd02a7753bcc394e26aa93b861169d37b12
doll2gom <shkim_ok@naver.com>
2023-05-26 11:38:08 +0900

    HH-24 #comment feat : posts 게시물 기본 구현

--- posts/urls.py
+++ posts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('posts/<int:post_pk>/', views.post_detail, name='post_detail'),
     path('posts/post_create/', views.post_create, name='post_create'),
-    path('posts/<int:post_pk>/post_update/', views.post_update, name='post_update'),
     path('posts/<int:post_pk>/post_delete/', views.post_delete, name='post_delete'),
+    path('posts/<int:post_pk>/post_update/', views.post_update, name='post_update'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -6,6 +6,7 @@ from .models import Post
 from .forms import PostForm
 
 # Create your views here.
+# 1 index
 def index(request):
     posts = Post.objects.all()
     context ={
@@ -13,7 +14,7 @@ def index(request):
     }
     return render(request,'posts/index.html', context)
 
-
+# 2 post_detail 내용 조회
 def post_detail(request, post_pk):
     post = Post.objects.get(pk=post_pk)
     context ={
@@ -21,7 +22,7 @@ def post_detail(request, post_pk):
     }
     return render(request,'posts/post_detail.html', context)
 
-
+# 3 post_create 생성
 @login_required
 def post_create(request):
     if request.method == 'POST':
@@ -38,15 +39,30 @@ def post_create(request):
     }
     return render(request,'posts/post_create.html', context)
 
-
+# 4 post_delete 삭제
 @login_required
 def post_delete(request, post_pk):
-    post = Article.objects.get(pk=artilce_pk)
-    if request.user == article.user:
-        article.delete()
-    return render(request, 'posts/post_delete.html', context)
+    post = Post.objects.get(pk=post_pk)
+    if request.user == post.user:
+        post.delete()
+    return redirect('posts:index')
 
-
+# 5 post_update 수정
 @login_required
 def post_update(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    if request.user == post.user:
+        if request.method == 'POST':
+            form = PostForm(request.POST, instance=post)
+            if form.is_valid():
+                form.save()
+                return redirect('posts:post_detail', post_pk)
+        else:
+            form = PostForm(instance=post)
+    else:
+        return redirect('posts:index')
+    context ={
+        'post': post,
+        'form': form,
+    }
     return render(request, 'posts/post_update.html', context)
