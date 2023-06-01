27976cc5e3635f32efadd905f62ff036b4e81dad
Kwonjunhyuk <wnsgur5529@naver.com>
2023-06-01 19:58:56 +0900

    HH-61 #comment feat : posts/models.py, forms.py 수정
    - posts/models.py : 필터링 기능을 위한 models 추가
    - posts/forms.py : models에 맞게 forms.py 추가

--- posts/forms.py
+++ posts/forms.py
@@ -30,9 +30,14 @@ class PostCommentForm(forms.ModelForm):
 
 
 class GroupForm(forms.ModelForm):
+    day = forms.ChoiceField(label='Day', choices=Group.day_choices, widget=forms.Select(attrs={'class': 'form-select'}),)
+    region = forms.ChoiceField(label='Region', choices=Group.region_choices, widget=forms.Select(attrs={'class': 'form-select'}),)
+    gender = forms.ChoiceField(label='Gender', choices=Group.gender_choices, widget=forms.Select(attrs={'class': 'form-select'}),)
+    Propensity = forms.ChoiceField(label='Propensity', choices=Group.Propensity_choices, widget=forms.Select(attrs={'class': 'form-select'}),)
+    category = forms.ChoiceField(label='Category', choices=Group.category_choices, widget=forms.Select(attrs={'class': 'form-select'}),)
     class Meta:
         model = Group
-        fields = ('title', 'content', 'address', 'category', 'tags',)
+        fields = ('title', 'content', 'tags', 'category',)
 
 
 class GroupImageFrom(forms.ModelForm):
