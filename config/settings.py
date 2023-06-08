6c0fddad4f048811dffdbef8e95367b7e813642d
zzgh06 <zzgh06@naver.com>
2023-06-08 15:06:59 +0900

    HH-73 #comment feat : HobbyHub 서비스 1차 배포
    - Update : config/settings.py DisallowedHost 오류 수정

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'port-0-hobbyhub-koh2xlimjxmaz.sel4.cloudtype.app',]
 
 
 # Application definition
