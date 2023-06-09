b68f7367b2f7239635c83c42ebc3f1469a7df1dc
nihelv <ys.chu0601@gmail.com>
2023-06-09 13:56:11 +0900

    HH-?? #comment fix profile.js
    -비밀번호 변경 뒤 리다이렉트 오류 views.py 수정
    -토큰 중복생성에 대한 profile.js 수정

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('posts.urls')),
+    path('accounts/', include('allauth.urls')),
     path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
